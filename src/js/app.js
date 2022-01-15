150342faff3e8ee1aecec8386204656bbf061569
Ihor Poliakov <48531156+junglq@users.noreply.github.com>
2022-01-15 15:49:32 +0200

    Delete src directory

--- src/js/app.js
+++
@@ -1,7 +0,0 @@
-import "../scss/style.scss";
-import "./libs/dynamic_adapt.js";
-import * as flsFunctions from "./files/functions.js";
-flsFunctions.isWebp();
-flsFunctions.menuInit();
-import "./files/sliders.js";
-import "./files/script.js";
