0fa3dd39aa8caee9adf0f6e87c0d370cb2c30808
Ihor Poliakov <48531156+junglq@users.noreply.github.com>
2022-01-15 15:44:49 +0200

    init

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+import "../scss/style.scss";
+import "./libs/dynamic_adapt.js";
+import * as flsFunctions from "./files/functions.js";
+flsFunctions.isWebp();
+flsFunctions.menuInit();
+import "./files/sliders.js";
+import "./files/script.js";
